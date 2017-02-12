c0ade1b9ac71aeed17c5d39b764bd09a2e16f1ca
132nd-etcher <132nd-etcher@daribouca.net>
2017-02-12 12:18:46 +0100

    added create_temp_* methods to __init__

--- utils/__init__.py
+++ utils/__init__.py
@@ -3,13 +3,14 @@
 from .custom_logging import make_logger, Logged
 from .validator import not_a_bool, not_a_positive_int, not_a_str, not_an_int, valid_bool, valid_float, valid_str, \
     valid_dict, valid_existing_path, valid_int, valid_list, valid_negative_int, valid_positive_int, Validator
-from .custom_path import Path
+from .custom_path import Path, create_temp_file, create_temp_dir
 from .downloader import Downloader
 from .progress import Progress, ProgressAdapter
 from .singleton import Singleton
 from .updater import Updater
 from .threadpool import ThreadPool
 from .decorators import TypedProperty
+from .pastebin import create_new_paste
 
 
 def nice_exit(*_):
