65641efbda0288a938f45cbeecb16a78ecdf45af
132nd-etcher <132nd-etcher@users.noreply.github.com>
2017-04-27 08:14:02 +0200

    Add time_perf deco
    From Fluent Python p222

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,5 +1,7 @@
 # coding=utf-8
 
+import time
+
 from .custom_logging import make_logger, Logged
 from .validator import not_a_bool, not_a_positive_int, not_a_str, not_an_int, valid_bool, valid_float, valid_str, \
     valid_dict, valid_existing_path, valid_int, valid_list, valid_negative_int, valid_positive_int, Validator
@@ -12,6 +14,18 @@ from .threadpool import ThreadPool
 from .decorators import TypedProperty
 from .pastebin import create_new_paste
 from .monkey import nice_exit
+
+
+def clock(func):
+    def clocked(*args): #
+        t0 = time.perf_counter()
+        result = func(*args) #
+        elapsed = time.perf_counter() - t0
+        name = func.__name__
+        arg_str = ', '.join(repr(arg) for arg in args)
+        print('[%0.8fs] %s(%s) -> %r' % (elapsed, name, arg_str, result))
+        return result
+    return clocked
 
 from ._version import get_versions
 __version__ = get_versions()['version']
